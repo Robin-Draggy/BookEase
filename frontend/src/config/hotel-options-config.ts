901039858fa13e8d2de9d191378209f1830c46d0
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2024-05-22 21:47:45 +0600

    completed the add hotel form design & also done the fetch code

---
+++ frontend/src/config/hotel-options-config.ts
@@ -0,0 +1,28 @@
+export const hotelTypes = [
+    "Budget",
+    "Boutique",
+    "Luxury",
+    "Ski Resort",
+    "Buisness",
+    "Family",
+    "Romantic",
+    "Hiking Resort",
+    "Cabin",
+    "Beach Resort",
+    "Motel",
+    "All Inclusive",
+    "Pet Friendly",
+    "Self Catering",
+];
+
+
+export const hotelFacilities = [
+    "Free Wifi",
+    "Parking",
+    "Airport Shuttle",
+    "Family Rooms",
+    "Non-Smoking Rooms",
+    "Outdor Pool",
+    "Spa",
+    "Fitness Center",
+]
