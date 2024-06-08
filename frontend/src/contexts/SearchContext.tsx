a3335b5b7d4035f26964a25d733eee3d111a4e84
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2024-06-09 02:43:13 +0600

    Added stripe and booking feature

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -28,20 +28,25 @@ export const SearchContextProvider = ({
   const [destination, setDestination] = useState<string>(
     () => sessionStorage.getItem("destination") || ""
   );
+
   const [checkIn, setCheckIn] = useState<Date>(
     () =>
       new Date(sessionStorage.getItem("checkIn") || new Date().toISOString())
   );
+
   const [checkOut, setCheckOut] = useState<Date>(
     () =>
       new Date(sessionStorage.getItem("checkOut") || new Date().toISOString())
   );
-  const [adultCount, setAdultCount] = useState<number>(() =>
+  
+    const [adultCount, setAdultCount] = useState<number>(() =>
     parseInt(sessionStorage.getItem("adultCount") || "1")
   );
+
   const [childCount, setChildCount] = useState<number>(() =>
     parseInt(sessionStorage.getItem("childCount") || "1")
   );
+  
   const [hotelId, setHotelId] = useState<string>(
     () => sessionStorage.getItem("hotelID") || ""
   );
