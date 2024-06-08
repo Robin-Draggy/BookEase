95cd686bbcca26e66b6bf60f2c518d5b8db3ac36
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2024-06-09 00:47:13 +0600

    Added view details page  and booking form

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -161,3 +161,13 @@ export const signOut = async () => {
   
     return response.json();
   };
+
+
+  export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
+    if (!response.ok) {
+      throw new Error("Error fetching Hotels");
+    }
+  
+    return response.json();
+  };
